525feb2f7bbd1f2d266da93d971c9dd7a0fe810a
santiagoswie2032 <vikalpbordekar@gmail.com>
2026-08-15 00:48:37 +0530

    hero content, hero text scroll content and titleSplit animations

--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -7,4 +7,14 @@ const HeroSection = () => {
     useGSAP((animation) => {
 
         const titleSplit = SplitText.create(".hero-title", {
-            type: "chars"
+            type: "chars"
+        });
+
+
+        // okay so the splitText will split block of words into individual
+        // words or chars to animate it 
+
+
+        const tl = gsap.timeline({
+            delay: 1,       //gsap timeline to animate the initial loading
+        });
