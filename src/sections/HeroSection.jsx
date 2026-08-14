ae27c463df5c05c37b231666015565ab1b9ef042
santiagoswie2032 <vikalpbordekar@gmail.com>
2026-08-15 00:48:44 +0530

    hero content, hero text scroll content and titleSplit animations

--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -57,4 +57,14 @@ const HeroSection = () => {
         <section className="bg-main-bg">
             <div className="hero-container">
                 <img
-                    src="/images/static-img.png"
+                    src="/images/static-img.png"
+                    alt="hero-img"
+                    className="absolute bottom-0 left-1/2 -translate-x-1/2 object-auto scale-100 md:scale-150"
+                />
+
+                <div className="hero-content" style={{ opacity: 0 }}>
+                    <div className="overflow-hidden">
+                        <h1 className="hero-title">Fcuking Delicious</h1>
+                    </div>
+
+                    <div
