fd6134110f32adf022167a2426ff21d91262dab8
santiagoswie2032 <vikalpbordekar@gmail.com>
2026-08-15 14:25:08 +0530

    finalized message section

--- src/sections/MessageSection.jsx
+++ src/sections/MessageSection.jsx
@@ -93,4 +93,36 @@ const MessageSection = () => {
 
     return (
         <section className='message-content'>
-            <div className='container mx-auto flex-center py-28 relative'>
+            <div className='container mx-auto flex-center py-28 relative'>
+                <div className='w-full h-full'>
+                    <div className='msg-wrapper'>
+                        <h1 className='first-message'>Stir Up Your Fearless Past and</h1>
+
+                        <div style={{
+                            clipPath: "polygon(0% 0%, 0% 0%, 0% 100%, 0% 100%)"
+                        }}
+                            className='msg-text-scroll mt-6 md:mt-10'>
+                            <div className="bg-light-brown md:pb-5 pb-3 px-5">
+                                <h2 className='text-red-brown'>Fuel Up Nigga</h2>
+                            </div>
+                        </div>
+
+                        <h1 className='second-message'>
+                            Your Future With Every Gulp Of Perfect Protein
+                        </h1>
+                    </div>
+                    <div className="flex-center md:mt-20 mt-10">
+                        <div className="max-w-md px-10 flex-center overflow-hidden">
+                            <p>
+                                Rev up your rebel spirit and feeed the adventure of life with splyt,
+                                where you're one chug away from epic nostalgia and fearless fun.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default MessageSection
