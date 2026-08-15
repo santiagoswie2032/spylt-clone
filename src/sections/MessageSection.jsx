707008c6f4fc75e43bf057184fa0c459639ed847
santiagoswie2032 <vikalpbordekar@gmail.com>
2026-08-15 12:40:33 +0530

    message section text's SplitText and reveal animations

--- src/sections/MessageSection.jsx
+++ src/sections/MessageSection.jsx
@@ -87,4 +87,14 @@ const MessageSection = () => {
                             className='msg-text-scroll mt-6 md:mt-10'>
                             <div className="bg-light-brown md:pb-5 pb-3 px-5">
                                 <h2 className='text-red-brown'>Fuel Up Nigga</h2>
-                            </div>
+                            </div>
+                        </div>
+
+                        <h1 className='second-message'>
+                            Your Future With Every Gulp Of Perfect Protein
+                        </h1>
+                    </div>
+                    <div className="flex-center md:mt-20 mt-10">
+                        <div className="max-w-md px-10 flex-center overflow-hidden">
+                            <p>
+                                Rev up your rebel spirit and feeed the adventure of life with splyt,
