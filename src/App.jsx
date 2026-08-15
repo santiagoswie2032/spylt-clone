7257760cba61ef40e915be46b7bf757167dcaba6
santiagoswie2032 <vikalpbordekar@gmail.com>
2026-08-15 08:58:02 +0530

    hero section rotation and message section animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import NavBar from './components/NavBar'
 import HeroSection from './sections/HeroSection'
+import { ScrollTrigger } from 'gsap/all';
+import gsap from "gsap";
+import MessageSection from './sections/MessageSection';
+
+
+gsap.registerPlugin(ScrollTrigger);
+// write registerPlugin once here and use it everywhere in the project
+// and use it everywhere in the project 
+
 
 const App = () => {
     return (
         <main>
             <NavBar />
             <HeroSection />
+            <MessageSection />
+            <div className='h-dvh border border-red-500'></div>
+
         </main>
     )
 }
