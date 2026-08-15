a9ce4ea46aad812ecf1e40535f87bf1920af7151
santiagoswie2032 <vikalpbordekar@gmail.com>
2026-08-15 08:58:26 +0530

    hero section rotation and message section animation

--- src/sections/MessageSection.jsx
+++ src/sections/MessageSection.jsx
@@ -27,4 +27,14 @@ const MessageSection = () => {
         <section className='message-content'>
             <div className='container mx-auto flex-center py-28 relative'>
                 <div className='w-full h-full'>
-                    <div className='msg-wrapper'>
+                    <div className='msg-wrapper'>
+                        <h1 className='first-message'>Stir Up Your Fearless Past and</h1>
+                        <div className='msg-text-scroll'>
+                            <div className="bg-light-brown md:pb-5 pb-3 px-5">
+                                <h2 className='text-red-brown'>Fuel Up</h2>
+                            </div>
+                        </div>
+
+                        <h1 className='second-message'>
+                            Your Future With Every Gulp Of Perfect Protein Nigga
+                        </h1>
