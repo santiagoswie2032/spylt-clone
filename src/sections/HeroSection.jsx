3af6d05cb0d77cd513aaecaf328887a9e9336805
santiagoswie2032 <vikalpbordekar@gmail.com>
2026-08-15 12:40:12 +0530

    message section text's SplitText and reveal animations

--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -67,4 +67,14 @@ const HeroSection = () => {
 
         heroTl.to(".hero-container", {
             rotate: 7,                  // rotate effect
-            scale: 0.8,                 // to shrink a lil bit
+            scale: 0.8,                 // to shrink a lil bit
+            yPercent: 30,               // come down a lil bit
+            ease: "power1.inOut"        // smooth in and out motion
+        })
+
+
+    });
+
+
+
+
