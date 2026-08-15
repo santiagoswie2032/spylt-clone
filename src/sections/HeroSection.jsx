769ff5e7ab27c17128f455447481b76a7b01a621
santiagoswie2032 <vikalpbordekar@gmail.com>
2026-08-15 12:40:14 +0530

    message section text's SplitText and reveal animations

--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -77,4 +77,14 @@ const HeroSection = () => {
 
 
 
-
+
+    return (
+        <section className="bg-main-bg">
+            <div className="hero-container">
+                <img
+                    src="/images/static-img.png"
+                    alt="hero-img"
+                    className="absolute bottom-0 left-1/2 -translate-x-1/2 object-auto scale-100 md:scale-150"
+                />
+
+                <div className="hero-content" style={{ opacity: 0 }}>
