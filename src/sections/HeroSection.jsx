376eb460511755e1a20ea3384d1e1bf31914848c
santiagoswie2032 <vikalpbordekar@gmail.com>
2026-08-15 12:40:07 +0530

    message section text's SplitText and reveal animations

--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -27,4 +27,14 @@ const HeroSection = () => {
             opacity: 1,                      // to fade in the content
             y: 0,                        // goes to 0 to its final position
             ease: "power1.inOut"           // smooth in and out motion
-        }).to('.hero-text-scroll', {
+        }).to('.hero-text-scroll', {
+            duration: 1,
+            clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+            ease: "circ.out"
+        }, "-=0.5").from(titleSplit.chars, {   // from because we are defining the initial position first here 
+            yPercent: 200,                      // the bottom where it should start from, 200% below it is here
+            stagger: 0.02,                       // time diff between popping up of each character so it creates wave like animation
+            ease: "power2.out"
+        }, "-=0.8")
+
+        //-=0.5 is the position parameter here which basially cancels out the default 0.5s duration
