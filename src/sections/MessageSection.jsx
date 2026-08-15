43ae72719ee34035434f231eefab67d5c410ee44
santiagoswie2032 <vikalpbordekar@gmail.com>
2026-08-15 12:40:30 +0530

    message section text's SplitText and reveal animations

--- src/sections/MessageSection.jsx
+++ src/sections/MessageSection.jsx
@@ -67,4 +67,14 @@ const MessageSection = () => {
             ease: "circ.inOut",
         })
 
-
+
+    });
+
+
+
+
+
+    return (
+        <section className='message-content'>
+            <div className='container mx-auto flex-center py-28 relative'>
+                <div className='w-full h-full'>
