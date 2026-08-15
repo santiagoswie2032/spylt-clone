132f29a43bc9009d6c4fba54df206de545481de1
santiagoswie2032 <vikalpbordekar@gmail.com>
2026-08-15 12:40:21 +0530

    message section text's SplitText and reveal animations

--- src/sections/MessageSection.jsx
+++ src/sections/MessageSection.jsx
@@ -7,4 +7,14 @@ import gsap from "gsap"
 const MessageSection = () => {
 
     useGSAP(() => {
-        const firstmsgSplit = SplitText.create('.first-message', {
+        const firstmsgSplit = SplitText.create('.first-message', {
+            type: 'words',
+        });
+
+        const secmsgSplit = SplitText.create('.second-message', {
+            type: 'words',
+        });
+
+        const paragraphSplit = SplitText.create('.message-content p', {
+            type: 'words, lines',
+            linesClass: "paragraph-line"
