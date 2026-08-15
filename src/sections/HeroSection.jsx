22b6a6edd76f9d835e380e0497c4b0a8e695777f
santiagoswie2032 <vikalpbordekar@gmail.com>
2026-08-15 08:58:12 +0530

    hero section rotation and message section animation

--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -72,4 +72,19 @@ const HeroSection = () => {
             ease: "power1.inOut"        // smooth in and out motion
         })
 
-
+
+    });
+
+
+
+
+    return (
+        <section className="bg-main-bg">
+            <div className="hero-container">
+                <img
+                    src="/images/static-img.png"
+                    alt="hero-img"
+                    className="absolute bottom-0 left-1/2 -translate-x-1/2 object-auto scale-100 md:scale-150"
+                />
+
+                <div className="hero-content" style={{ opacity: 0 }}>
