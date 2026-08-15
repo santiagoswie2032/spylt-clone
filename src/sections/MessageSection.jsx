0128514673ba3419b94a5d211f6cd1cf679120d4
santiagoswie2032 <vikalpbordekar@gmail.com>
2026-08-15 08:58:22 +0530

    hero section rotation and message section animation

--- src/sections/MessageSection.jsx
+++ src/sections/MessageSection.jsx
@@ -7,4 +7,14 @@ const MessageSection = () => {
         const firstmsgSplit = SplitText.create('.first-message', {
             type: 'words',
         });
+
+        const secmsgSplit = SplitText.create('.second-message', {
+            type: 'words',
+        });
+
+        const paragraphSplit = SplitText.create('.message-content p', {
+            type: 'words, lines',
+            linesClass: paragraph - line
+        });
+
 
