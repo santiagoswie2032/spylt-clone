72d720c4d5eb3e1e80ccba8b5763a314a8d06b5a
santiagoswie2032 <vikalpbordekar@gmail.com>
2026-08-17 10:17:45 +0530

    flavorsection, flavorslider, flavortitle

--- src/sections/FlavorSection.jsx
+++ src/sections/FlavorSection.jsx
@@ -5,4 +5,8 @@ import FlavorSlider from '../components/FlavorSlider'
 const FlavorSection = () => {
     return <section className='flavor-section'>
         <div className='h-full flex lg:flex-row flex-col items-center relative'>
-            <div className='lg:w-[57%] flex-none h-80 lg:h-full md:mt-20 xl:mt-0'>
+            <div className='lg:w-[57%] flex-none h-80 lg:h-full md:mt-20 xl:mt-0'>
+                <FlavorTitle />
+            </div>
+            <div className='h-full'></div>
+            <FlavorSlider />
