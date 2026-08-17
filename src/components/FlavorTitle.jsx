80ff7a85aafd23d3b03525f8645965619cbfd62b
santiagoswie2032 <vikalpbordekar@gmail.com>
2026-08-17 10:17:53 +0530

    flavorsection, flavorslider, flavortitle

--- src/components/FlavorTitle.jsx
+++ src/components/FlavorTitle.jsx
@@ -1,4 +1,8 @@
 
 
 const FlavorTitle = () => {
-    return (
+    return (
+        <div className="general-title col-center h-full 2xl:gap-32 xl:gap-24 gap-16">
+            <div className="overflow-hidden 2xl:py-0 py-3 first-text-split">
+                <h1>We have 6</h1>
+            </div>
