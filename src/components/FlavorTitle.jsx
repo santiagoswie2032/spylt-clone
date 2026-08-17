b4af6558fe4b139bee03c4188ed60e47583cd621
santiagoswie2032 <vikalpbordekar@gmail.com>
2026-08-17 20:15:15 +0530

    FlavorTitle done, next is FlavorSlider

--- src/components/FlavorTitle.jsx
+++ src/components/FlavorTitle.jsx
@@ -1,19 +1,3 @@
-
-
-const FlavorTitle = () => {
-    return (
-        <div className="general-title col-center h-full 2xl:gap-32 xl:gap-24 gap-16">
-            <div className="overflow-hidden 2xl:py-0 py-3 first-text-split">
-                <h1>We have 6</h1>
-            </div>
-
-            <div style={{}} className="flavor-text-scroll">
-                <div className="bg-mid-brown pb-5 2xl:pt-0 pt-3 2xl:px-5 px-3">
-                    <h2 className="text-milk">Fcuking</h2>
-                </div>
-            </div>
-        </div>
-    )
-}
-
-export default FlavorTitle
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap";
+import SplitText from "gsap/SplitText.js";
