d61c705a3f6f09dfa8ac0972d7d727bce77666a3
santiagoswie2032 <vikalpbordekar@gmail.com>
2026-08-18 10:10:11 +0530

    adding horizontal scroll flavor slider

--- src/constants/index.js
+++ src/constants/index.js
@@ -77,4 +77,14 @@ const cards = [
         src: "/videos/f6.mp4",
         rotation: "rotate-z-[4deg]",
         name: "Devante",
-        img: "/images/p6.png",
+        img: "/images/p6.png",
+        translation: "translate-y-[5%]",
+    },
+    {
+        src: "/videos/f7.mp4",
+        rotation: "rotate-z-[-3deg]",
+        name: "Melisa",
+        img: "/images/p7.png",
+        translation: "translate-y-[10%]",
+    },
+];
