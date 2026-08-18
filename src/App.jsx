7aa7f435892976a05e5952934afae35db07c9dfa
santiagoswie2032 <vikalpbordekar@gmail.com>
2026-08-18 20:38:00 +0530

    horizontal flavorSlider done, ScrollSmoother plugin has been added, next is parallax effect on FlavorTitle

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,10 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import NavBar from './components/NavBar'
 import HeroSection from './sections/HeroSection'
-import { ScrollTrigger } from 'gsap/all';
+import { ScrollTrigger, ScrollSmoother } from 'gsap/all';
 import gsap from "gsap";
 import MessageSection from './sections/MessageSection';
 import FlavorSection from './sections/FlavorSection';
 import FlavorSlider from './components/FlavorSlider';
-
-
-gsap.registerPlugin(ScrollTrigger);
-// write registerPlugin once here and use it everywhere in the project
-// and use it everywhere in the project 
-
-
-const App = () => {
-    return (
-        <main>
-            <NavBar />
-            <HeroSection />
-            <MessageSection />
-            <FlavorSection />
-            <FlavorSlider />
-            <div className='h-dvh border border-red-500'></div>
-
-        </main>
-    )
-}
-
-
-export default App
+import { useGSAP } from '@gsap/react';
+
