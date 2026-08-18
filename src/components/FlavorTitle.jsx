4d0121ee7b175b13703be64a40259883a3b4de60
santiagoswie2032 <vikalpbordekar@gmail.com>
2026-08-18 20:38:15 +0530

    horizontal flavorSlider done, ScrollSmoother plugin has been added, next is parallax effect on FlavorTitle

--- src/components/FlavorTitle.jsx
+++ src/components/FlavorTitle.jsx
@@ -13,4 +13,20 @@ const FlavorTitle = () => {
         const secText = SplitText.create('.second-text-split h1', {
             type: "chars",
         });
-
+
+
+
+        gsap.from(firstText.chars, {
+            yPercent: 200,
+            stagger: 0.02,
+            duration: 0.8,
+
+            scrollTrigger: {
+                trigger: ".first-text-split h1",
+                start: "top 100%"
+            }
+
+        })
+
+        gsap.from(secText.chars, {
+            yPercent: 200,
