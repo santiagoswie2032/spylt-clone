127a58937d9f12057b003807a2ba141ac61f758f
santiagoswie2032 <vikalpbordekar@gmail.com>
2026-08-18 20:38:34 +0530

    horizontal flavorSlider done, ScrollSmoother plugin has been added, next is parallax effect on FlavorTitle

--- src/sections/MessageSection.jsx
+++ src/sections/MessageSection.jsx
@@ -9,4 +9,16 @@ const MessageSection = () => {
     useGSAP(() => {
         const firstmsgSplit = SplitText.create('.first-message', {
             type: 'words',
-        });
+        });
+
+        const secmsgSplit = SplitText.create('.second-message', {
+            type: 'words',
+        });
+
+        const paragraphSplit = SplitText.create('.message-content p', {
+            type: 'words, lines',
+            linesClass: "paragraph-line"
+        });
+
+        gsap.to(firstmsgSplit.words, {
+            color: "#faeade",
