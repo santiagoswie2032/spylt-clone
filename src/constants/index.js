b77e55df0ba2a877fb01ddb9523c3f26d01300a6
santiagoswie2032 <vikalpbordekar@gmail.com>
2026-08-18 10:10:04 +0530

    adding horizontal scroll flavor slider

--- src/constants/index.js
+++ src/constants/index.js
@@ -7,4 +7,14 @@ const flavorlists = [
     {
         name: "Stawberry Milk",
         color: "red",
+        rotation: "md:rotate-[8deg] rotate-0",
+    },
+    {
+        name: "Cookies & Cream",
+        color: "blue",
+        rotation: "md:rotate-[-8deg] rotate-0",
+    },
+    {
+        name: "Peanut Butter Chocolate",
+        color: "orange",
         rotation: "md:rotate-[8deg] rotate-0",
