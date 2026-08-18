4d48f8c9c4bbbd96d8373adc4dc94e4a7335bd7a
santiagoswie2032 <vikalpbordekar@gmail.com>
2026-08-18 20:38:42 +0530

    horizontal flavorSlider done, ScrollSmoother plugin has been added, next is parallax effect on FlavorTitle

--- src/sections/MessageSection.jsx
+++ src/sections/MessageSection.jsx
@@ -81,4 +81,16 @@ const MessageSection = () => {
             stagger: 0.02,                          // duration between popping up of each word
             rotate: 15,                              // starts from a 3 degree rotation back to 0 deg to give that rotating effect
             ease: 'power1.inOut',                   // smooth effect
-            yPercent: 300                           // where should it start from ? remember, its a reveal animation 
+            yPercent: 300                           // where should it start from ? remember, its a reveal animation 
+        })                                          // so you have to hide it first, then it reveals itself
+
+
+    });
+
+
+
+
+
+    return (
+        <section className='message-content'>
+            <div className='container mx-auto flex-center py-28 relative'>
