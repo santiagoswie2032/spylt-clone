470e7e2de38fece3ef188ddc9392e684a23193e9
santiagoswie2032 <vikalpbordekar@gmail.com>
2026-08-20 00:01:41 +0530

    parallax effect done

--- src/components/FlavorSlider.jsx
+++ src/components/FlavorSlider.jsx
@@ -1,4 +1,8 @@
 import { useGSAP } from "@gsap/react";
 import { flavorlists } from "../constants";
 import gsap from "gsap";
-import { useRef } from "react";
+import { useRef } from "react";
+
+const FlavorSlider = () => {
+    const sliderRef = useRef();
+
