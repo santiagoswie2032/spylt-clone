a9ecda39fc47f3eccc2ba460afdbd5fccb5cdfa0
santiagoswie2032 <vikalpbordekar@gmail.com>
2026-08-20 00:02:06 +0530

    parallax effect done

--- src/components/FlavorSlider.jsx
+++ src/components/FlavorSlider.jsx
@@ -65,4 +65,8 @@ const FlavorSlider = () => {
                     return (<div
                         key={value.name}
                         className={`relative z-30 lg:w-[50vw] w-96 lg:h-[70vh] md:w-[90vw] h-80 flex-none ${value.rotation}`}
-                    >
+                    >
+                        <img src={`/images/${value.color}-bg.svg`} alt="" className="absolute bottom-0 " />
+                        <img src={`/images/${value.color}-drink.webp`} className="drinks" />
+                        <img src={`/images/${value.color}-elements.webp`} alt="" className="elements" />
+                        <h1>{value.name}</h1>
