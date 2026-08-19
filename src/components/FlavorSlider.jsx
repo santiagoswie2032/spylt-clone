4e34edc1c2a66767a8442f318e45d5d556509f01
santiagoswie2032 <vikalpbordekar@gmail.com>
2026-08-20 00:02:05 +0530

    parallax effect done

--- src/components/FlavorSlider.jsx
+++ src/components/FlavorSlider.jsx
@@ -61,4 +61,8 @@ const FlavorSlider = () => {
     return (
         <div ref={sliderRef} className="slider-wrapper">
             <div className="flavors">
-                {flavorlists.map((value) => {
+                {flavorlists.map((value) => {
+                    return (<div
+                        key={value.name}
+                        className={`relative z-30 lg:w-[50vw] w-96 lg:h-[70vh] md:w-[90vw] h-80 flex-none ${value.rotation}`}
+                    >
