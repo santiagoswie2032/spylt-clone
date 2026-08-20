36f17ba6481750a77b4c0a9f75aed5f7096f8b54
santiagoswie2032 <vikalpbordekar@gmail.com>
2026-08-20 10:12:02 +0530

    half of nutrition section is done, gotta go to college, will do ts after i get back

--- src/sections/NutritionSection.jsx
+++ src/sections/NutritionSection.jsx
@@ -9,4 +9,10 @@ const NutritionSection = () => {
 
             <div className='flex md:flex-row flex-col justify-between md:px-10 px-5 mt-14 md:mt-0'>
                 <div className='relative inline-block md:translate-y-20'>
-                    <div className='general-title relative flex flex-col justify-center gap-24'>
+                    <div className='general-title relative flex flex-col justify-center gap-24'>
+                        <div className='overflow-hidden place-self-start'>
+                            <h1>It still does</h1>
+                        </div>
+                        <div style={{}} className='nutrition-text-scroll place-self-start'>
+                            <div className='bg-yellow-brown pb-5 md:pt-0 pt-3 md:px-5 px-3 inline-block'></div>
+                        </div>
