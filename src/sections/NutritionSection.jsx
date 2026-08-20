d1a43efd9f534eb6c88f96bbe87eb99cdb8c677e
santiagoswie2032 <vikalpbordekar@gmail.com>
2026-08-20 23:18:59 +0530

    Nutrition section is done and done

--- src/sections/NutritionSection.jsx
+++ src/sections/NutritionSection.jsx
@@ -75,4 +75,17 @@ const NutritionSection = () => {
             <img src="/images/big-img.png" alt="" className="big-img" />
 
             <div className="flex md:flex-row flex-col justify-between md:px-10 px-5 mt-14 md:mt-0">
-                <div className="relative inline-block md:translate-y-20">
+                <div className="relative inline-block md:translate-y-20">
+                    <div className="general-title relative flex flex-col justify-center items-center gap-24">
+                        <div className="overflow-hidden place-self-start">
+                            <h1 className="nutrition-title">It still does</h1>
+                        </div>
+                        <div
+                            style={{
+                                clipPath: "polygon(0 0, 0 0, 0 100%, 0% 100%)",
+                            }}
+                            className="nutrition-text-scroll place-self-start"
+                        >
+                            <div className="bg-yellow-brown pb-5 md:pt-0 pt-3 md:px-5 px-3">
+                                <h2 className="text-milk-yellow">Body Good</h2>
+                            </div>
