bb447551faa15a69b629ea7a488fb3a628cbff65
santiagoswie2032 <vikalpbordekar@gmail.com>
2026-08-20 10:12:00 +0530

    half of nutrition section is done, gotta go to college, will do ts after i get back

--- src/sections/NutritionSection.jsx
+++ src/sections/NutritionSection.jsx
@@ -3,4 +3,10 @@ import React from 'react'
 const NutritionSection = () => {
     return (
         <section className='nutrition-section'>
-            <img src="/images/slider-dip.png" alt="" className='w-full object-cover' />
+            <img src="/images/slider-dip.png" alt="" className='w-full object-cover' />
+
+            <img src="/images/big-img.png" alt="" />
+
+            <div className='flex md:flex-row flex-col justify-between md:px-10 px-5 mt-14 md:mt-0'>
+                <div className='relative inline-block md:translate-y-20'>
+                    <div className='general-title relative flex flex-col justify-center gap-24'>
