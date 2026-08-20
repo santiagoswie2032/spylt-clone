41ba388aa9b99f06f2ea5475479dfe98ccf821ce
santiagoswie2032 <vikalpbordekar@gmail.com>
2026-08-20 23:18:58 +0530

    Nutrition section is done and done

--- src/sections/NutritionSection.jsx
+++ src/sections/NutritionSection.jsx
@@ -62,4 +62,17 @@ const NutritionSection = () => {
             clipPath: "polygon(100% 0, 0 0, 0 100%, 100% 100%)",
             ease: "power1.inOut",
         });
-    });
+    });
+
+    return (
+        <section className="nutrition-section">
+            <img
+                src="/images/slider-dip.png"
+                alt=""
+                className="w-full object-cover"
+            />
+
+            <img src="/images/big-img.png" alt="" className="big-img" />
+
+            <div className="flex md:flex-row flex-col justify-between md:px-10 px-5 mt-14 md:mt-0">
+                <div className="relative inline-block md:translate-y-20">
