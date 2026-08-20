b9c7475326cb923c6bf38ce9e1974bd6439cb287
santiagoswie2032 <vikalpbordekar@gmail.com>
2026-08-20 10:11:59 +0530

    half of nutrition section is done, gotta go to college, will do ts after i get back

---
+++ src/sections/NutritionSection.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const NutritionSection = () => {
+    return (
+        <section className='nutrition-section'>
+            <img src="/images/slider-dip.png" alt="" className='w-full object-cover' />
