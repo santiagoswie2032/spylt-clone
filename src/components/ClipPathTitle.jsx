e82cb666ff4443c33961602486b8172b3f53bd71
santiagoswie2032 <vikalpbordekar@gmail.com>
2026-08-21 09:43:58 +0530

    benefit section done, benefit section video reveal animation done, hero section video done

---
+++ src/components/ClipPathTitle.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const ClipPathTitle = ({ title, color, bg, className, borderColor }) => {
+    return (
+        <div className='general-title'>
+            <div style={{
