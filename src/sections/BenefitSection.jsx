fbead8722dfcd5bd473d897099a6dfaef2fabe93
santiagoswie2032 <vikalpbordekar@gmail.com>
2026-08-21 09:44:18 +0530

    benefit section done, benefit section video reveal animation done, hero section video done

--- src/sections/BenefitSection.jsx
+++ src/sections/BenefitSection.jsx
@@ -3,4 +3,10 @@ import ClipPathTitle from '../components/ClipPathTitle'
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
 import VIdeoPinSection from '../components/VIdeoPinSection'
-
+
+
+const BenefitSection = () => {
+
+
+    useGSAP(() => {
+        const RevealTl = gsap.timeline({
