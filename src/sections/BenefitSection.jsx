3fb0e8e5959cb960fc687a5fc0b7489108749c0e
santiagoswie2032 <vikalpbordekar@gmail.com>
2026-08-21 09:44:17 +0530

    benefit section done, benefit section video reveal animation done, hero section video done

---
+++ src/sections/BenefitSection.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import ClipPathTitle from '../components/ClipPathTitle'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import VIdeoPinSection from '../components/VIdeoPinSection'
+
