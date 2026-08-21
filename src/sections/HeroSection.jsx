48840de59999eb241b67698ddfe107ee8f9c98c8
santiagoswie2032 <vikalpbordekar@gmail.com>
2026-08-21 09:44:57 +0530

    benefit section done, benefit section video reveal animation done, hero section video done

--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -117,4 +117,14 @@ const HeroSection = () => {
                         <div className="hero-subtitle">
                             <h1>Protein + Caffine</h1>
                         </div>
-                    </div>
+                    </div>
+
+                    <h2>
+                        Live Life to the fullest with SPYLT: Shatter boredom and
+                        embrace your inner child with every delicously smooth chug.
+                    </h2>
+
+                    <div>
+                        <button className="hero-button">CHUG A SPYLT</button>
+                    </div>
+                </div>
