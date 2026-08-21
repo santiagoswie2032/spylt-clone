4cb204360f68876e46fc932a8eb7f73c6dc4d56e
santiagoswie2032 <vikalpbordekar@gmail.com>
2026-08-21 09:44:46 +0530

    benefit section done, benefit section video reveal animation done, hero section video done

--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -27,4 +27,14 @@ const HeroSection = () => {
 
         const tl = gsap.timeline({
             delay: 1,       //gsap timeline to animate the initial loading
-        });
+        });
+
+
+
+        tl.to('.hero-content', {
+
+            opacity: 1,                      // to fade in the content
+            y: 0,                        // goes to 0 to its final position
+            ease: "power1.inOut"           // smooth in and out motion
+        }).to('.hero-text-scroll', {
+            duration: 0.8,
