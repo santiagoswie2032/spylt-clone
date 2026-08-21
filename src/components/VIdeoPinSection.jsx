2150f06c80d02a32395d618f4912e4b81a0b7caf
santiagoswie2032 <vikalpbordekar@gmail.com>
2026-08-21 09:44:06 +0530

    benefit section done, benefit section video reveal animation done, hero section video done

--- src/components/VIdeoPinSection.jsx
+++ src/components/VIdeoPinSection.jsx
@@ -2,4 +2,9 @@ import { useGSAP } from "@gsap/react"
 import gsap from "gsap"
 
 const VIdeoPinSection = () => {
-
+
+
+    useGSAP(() => {
+
+        const tl = gsap.timeline({
+            scrollTrigger: {
