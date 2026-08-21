9fc98e8146ceef6d176c2e930f43aacbb61a8211
santiagoswie2032 <vikalpbordekar@gmail.com>
2026-08-21 09:43:54 +0530

    benefit section done, benefit section video reveal animation done, hero section video done

--- src/App.jsx
+++ src/App.jsx
@@ -30,4 +30,15 @@ const App = () => {
     return (
         <main>
             <NavBar />
+
+            <div id="smooth-wrapper">
+                <div id="smooth-content">
+                    <HeroSection />
+                    <MessageSection />
+                    <FlavorSection />
+                    <NutritionSection />
+                    <BenefitSection />
+                </div>
+            </div>
+
 
