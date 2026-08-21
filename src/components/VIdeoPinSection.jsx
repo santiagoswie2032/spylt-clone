f2b46a2fe60cdedd8396a52692a5189c3c3de7e6
santiagoswie2032 <vikalpbordekar@gmail.com>
2026-08-21 09:44:07 +0530

    benefit section done, benefit section video reveal animation done, hero section video done

--- src/components/VIdeoPinSection.jsx
+++ src/components/VIdeoPinSection.jsx
@@ -7,4 +7,9 @@ const VIdeoPinSection = () => {
     useGSAP(() => {
 
         const tl = gsap.timeline({
-            scrollTrigger: {
+            scrollTrigger: {
+                trigger: '.vd-pin-section',
+                start: 'top top',
+                end: '200%',
+                scrub: 1.5,
+                pin: true,
