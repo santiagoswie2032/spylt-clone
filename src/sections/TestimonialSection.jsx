208f4a00ef7514ffc56cc4239c07778d4be0de75
santiagoswie2032 <vikalpbordekar@gmail.com>
2026-08-26 17:08:56 +0530

    Testimonial section, not done yet

--- src/sections/TestimonialSection.jsx
+++ src/sections/TestimonialSection.jsx
@@ -1,3 +1,6 @@
 
 const TestimonialSection = () => {
-    return (
+    return (
+        <section className="testimonials-section">
+            <div className="absolute size-full flex flex-col items-center pt-[5vw]">
+                <h1 className="text-black first-title">What's</h1>
