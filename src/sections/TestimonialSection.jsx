9e2c7ce47ab7317e8c222978bd6af9fb73af1c26
santiagoswie2032 <vikalpbordekar@gmail.com>
2026-08-26 17:08:57 +0530

    Testimonial section, not done yet

--- src/sections/TestimonialSection.jsx
+++ src/sections/TestimonialSection.jsx
@@ -3,4 +3,7 @@ const TestimonialSection = () => {
     return (
         <section className="testimonials-section">
             <div className="absolute size-full flex flex-col items-center pt-[5vw]">
-                <h1 className="text-black first-title">What's</h1>
+                <h1 className="text-black first-title">What's</h1>
+                <h1 className="text-light-brown sec-title">everyone</h1>
+                <h1 className="text third-title">talking</h1>
+            </div>
