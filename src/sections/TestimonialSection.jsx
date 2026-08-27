55bd9e5d4e725a0c7fb76607fd555034218364cc
santiagoswie2032 <vikalpbordekar@gmail.com>
2026-08-28 00:03:18 +0530

    Testimonial section, video cards done, still not done yet

--- src/sections/TestimonialSection.jsx
+++ src/sections/TestimonialSection.jsx
@@ -1,17 +1,1 @@
-
-const TestimonialSection = () => {
-    return (
-        <section className="testimonials-section">
-            <div className="absolute size-full flex flex-col items-center pt-[5vw]">
-                <h1 className="text-black first-title">What's</h1>
-                <h1 className="text-light-brown sec-title">everyone</h1>
-                <h1 className="text third-title">talking</h1>
-            </div>
-
-            <div className="pin-box"></div>
-
-        </section>
-    )
-}
-
-export default TestimonialSection
+import { cards } from "../constants"
