313fc50c782979687906edef9de29ab1a6eb4fb0
santiagoswie2032 <vikalpbordekar@gmail.com>
2026-08-28 00:03:24 +0530

    Testimonial section, video cards done, still not done yet

--- src/sections/TestimonialSection.jsx
+++ src/sections/TestimonialSection.jsx
@@ -2,4 +2,5 @@ import { cards } from "../constants"
 
 const TestimonialSection = () => {
     return (
-        <section className="testimonials-section">
+        <section className="testimonials-section">
+            <div className="absolute size-full flex flex-col items-center pt-[5vw]">
