bd88361fbf291afff9c93366e7e1ce4d0857cee9
santiagoswie2032 <vikalpbordekar@gmail.com>
2026-08-28 00:03:28 +0530

    Testimonial section, video cards done, still not done yet

--- src/sections/TestimonialSection.jsx
+++ src/sections/TestimonialSection.jsx
@@ -4,4 +4,5 @@ const TestimonialSection = () => {
     return (
         <section className="testimonials-section">
             <div className="absolute size-full flex flex-col items-center pt-[5vw]">
-                <h1 className="text-black first-title">What's</h1>
+                <h1 className="text-black first-title">What's</h1>
+                <h1 className="text-light-brown sec-title">everyone</h1>
