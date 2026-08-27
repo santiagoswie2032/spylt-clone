a3f6c3eb3c91a9e54d2c4573357234d872cb0603
santiagoswie2032 <vikalpbordekar@gmail.com>
2026-08-28 00:03:32 +0530

    Testimonial section, video cards done, still not done yet

--- src/sections/TestimonialSection.jsx
+++ src/sections/TestimonialSection.jsx
@@ -7,4 +7,5 @@ const TestimonialSection = () => {
                 <h1 className="text-black first-title">What's</h1>
                 <h1 className="text-light-brown sec-title">everyone</h1>
                 <h1 className="text third-title">talking</h1>
-            </div>
+            </div>
+
