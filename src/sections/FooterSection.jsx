c05657acdaeb1a34a625542e5c72dfb1b5370fab
santiagoswie2032 <vikalpbordekar@gmail.com>
2026-08-29 19:47:19 +0530

    Testimonial section done, footer section done

--- src/sections/FooterSection.jsx
+++ src/sections/FooterSection.jsx
@@ -2,4 +2,9 @@ import { useMediaQuery } from "react-responsive";
 
 const FooterSection = () => {
     const isMobile = useMediaQuery({
-        query: "(max-width: 768px)",
+        query: "(max-width: 768px)",
+    });
+
+    return (
+        <section className="footer-section">
+            <img
