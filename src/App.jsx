d0fc98e9593e2e72e9264a2137be310be767fd17
santiagoswie2032 <vikalpbordekar@gmail.com>
2026-08-29 19:46:31 +0530

    Testimonial section done, footer section done

--- src/App.jsx
+++ src/App.jsx
@@ -10,3 +10,15 @@ import { useGSAP } from '@gsap/react';
 import NutritionSection from './sections/NutritionSection';
 import BenefitSection from './sections/BenefitSection';
 import TestimonialSection from './sections/TestimonialSection';
+import FooterSection from './sections/FooterSection';
+
+
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+// write registerPlugin once here and use it everywhere in the project
+// and use it everywhere in the project 
+
+
+const App = () => {
+
+    useGSAP(() => {
