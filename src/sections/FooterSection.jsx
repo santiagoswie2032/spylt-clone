00fe1e3b49370d87ece5d2907fdd2c590e664068
santiagoswie2032 <vikalpbordekar@gmail.com>
2026-08-29 19:47:21 +0530

    Testimonial section done, footer section done

--- src/sections/FooterSection.jsx
+++ src/sections/FooterSection.jsx
@@ -12,4 +12,9 @@ const FooterSection = () => {
                 alt=""
                 className="w-full object-cover -translate-y-1"
             />
-
+
+            <div className="2xl:h-[110dvh] relative md:pt-[20vh] pt-[10vh]">
+                <div className="overflow-hidden z-10">
+                    <h1 className="general-title text-center text-milk py-5">
+                        #CHUGRESPONSIBLY
+                    </h1>
