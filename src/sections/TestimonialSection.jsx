5d143bccffd26b18b0c0a2aedc9844268ba6b09e
santiagoswie2032 <vikalpbordekar@gmail.com>
2026-08-29 19:47:11 +0530

    Testimonial section done, footer section done

--- src/sections/TestimonialSection.jsx
+++ src/sections/TestimonialSection.jsx
@@ -52,4 +52,9 @@ const TestimonialSection = () => {
 
         })
     });
-
+
+
+
+    return (
+        <section className="testimonials-section">
+            <div className="absolute size-full flex flex-col items-center pt-0 -mt-10">
