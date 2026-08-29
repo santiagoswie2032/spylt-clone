8e95ccf391ff1572baa59d933b3114b640d510a3
santiagoswie2032 <vikalpbordekar@gmail.com>
2026-08-29 19:47:00 +0530

    Testimonial section done, footer section done

--- src/sections/TestimonialSection.jsx
+++ src/sections/TestimonialSection.jsx
@@ -2,4 +2,9 @@ import { useGSAP } from "@gsap/react"
 import { cards } from "../constants"
 import gsap from "gsap"
 
-const TestimonialSection = () => {
+const TestimonialSection = () => {
+
+
+    useGSAP(() => {
+        gsap.set('.testimonials-section', {
+            marginTop: "-100vh",
